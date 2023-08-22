b1ea2ea3d3690b0993edfdd8ecfbfd5c7a88b8b9
HannesTinnis <hannes.tinnis@elev.ga.ntig.se>
2023-08-22 09:43:28 +0200

    jag uppdaterade kanpen

---
+++ intro.py
@@ -0,0 +1,26 @@
+print("Hello World")
+print('"hello world"')
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum 
+
+""")   
+
+print("hej","hej")
+
+x = input("Vad heter du? ")
+print("Hej " + x )
+lol = input("hur gammal är du? ")
+xx = input("vad är din favorit färg? ")
+print("Hej " + x)
+print("Du är "+ lol, "år gammal")
+print("Och din favorit färg är " + xx)
+
